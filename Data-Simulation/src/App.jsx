b8d22176a89584cf230233f5139b1b85d7f1026b
Belaid Meriem <128685620+belaid1meriem@users.noreply.github.com>
2025-02-02 17:31:43 +0100

    Correcting Icons problem

--- Data-Simulation/src/App.jsx
+++ Data-Simulation/src/App.jsx
@@ -9,6 +9,24 @@ import ErrorMsg from './components/ErrorMsg/ErrorMsg.jsx'
 import SuccessMsg from './components/SuccessMsg/SuccessMsg.jsx'
 import InfoMsg from './components/InfoMsg/InfoMsg.jsx'
 
+//leaflet
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// Import images
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// Override the default icon settings
+delete L.Icon.Default.prototype._getIconUrl; 
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 
 function App() {
   const [visible, setVisible]= useState(true);
